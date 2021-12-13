069e82ff57a4f0fd84db4fe8239050ea74f06c11
juliatengner <93980693+juliatengner@users.noreply.github.com>
2021-12-13 13:18:53 +0100

    spara

--- src/screens/GasFuel.js
+++ src/screens/GasFuel.js
@@ -12,6 +12,7 @@ import {
   Tooltip,
 } from "recharts";
 import bakgrund1 from "../Images/back-co2.png";
+import AboutGasFuel from "../components/About"
 
 const GasFuel = (props) => {
   // Applikationens innehåll med förklarande text samt en line chart
@@ -23,22 +24,7 @@ const GasFuel = (props) => {
         style={{ backgroundImage: `url(${bakgrund1})` }}
       >
         <Col xs={{ span: 6, offset: 1 }} className="pe-5 pt-4 overlay-text ">
-          <h1>Gasbränsle</h1>
-          <p>Gasbränsle innefattar bränslen i gasform. </p>
-          <p>
-            Exempel på gasbränslen är till exempel fordonsgas vilket är ett
-            drivmedel som består av biogas, naturgas eller kombinationer av de
-            båda.
-          </p>
-          <p>
-            Biogas är ett förnybart bränsle som ger mycket mindre utsläpp av
-            koldioxid än bensin och diesel.
-          </p>
-          <p>
-            Naturgas är ett fossilt (icke förnybart) bränsle som utvinns ur
-            olja. Att köra bil på naturgas ger större utsläpp av växthusgaser än
-            biogas men lägre än bensin och diesel.
-          </p>
+        <AboutGasFuel />
         </Col>
         <div className="wrapper overlay-graf">
           <ResponsiveContainer width="100%" height="80%">
