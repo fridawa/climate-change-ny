7d0934ffb8895a0f8ee8f93a263e484ba5a7d61e
Frida Wallgren <58770144+fridawa@users.noreply.github.com>
2021-12-21 18:07:38 +0100

    tog bort margin på body iapp.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
-// footer component in the App.js component 
+// footer component in the App.js component
 
 const Footer = () => {
   return (
-    <div style={{bottom: "0em", right: ".5em", color:"rgb(158, 145, 127)", position:"fixed"}}>
+    <div
+      style={{
+        bottom: "0em",
+        right: ".5em",
+        color: "rgb(158, 145, 127)",
+        position: "fixed",
+      }}
+    >
       <p>Skapad av Frida, Henna och Julia</p>
     </div>
   );
